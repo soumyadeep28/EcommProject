d85a8b24d890b3ff877df11ae2d163424e4e3cd6
ssengup <ssengup@gmail>
2022-01-27 08:13:56 +0530

    modified all the basic componenet and upgraded the setting and included the templates in the application level and rendered that accordingly

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>This is a home page of blog</h1>")
+    return render(request , 'blog/index.html')
--- shop/views.py
+++ shop/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1> hellooo guys</h1> ")
+    return render(request , 'shop/index.html')
